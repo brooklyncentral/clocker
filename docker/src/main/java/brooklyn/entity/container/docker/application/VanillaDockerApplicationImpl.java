f3dc37b94bc31ada2b3f2ff705e86fee3ecb69d5
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-05-31 15:54:51 +0100

    Added simple vanilla docker application

---
+++ docker/src/main/java/brooklyn/entity/container/docker/application/VanillaDockerApplicationImpl.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2014 by Cloudsoft Corporation Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package brooklyn.entity.container.docker.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import brooklyn.entity.basic.VanillaSoftwareProcessImpl;
+import brooklyn.entity.container.docker.DockerContainer;
+import brooklyn.entity.container.docker.DockerHost;
+import brooklyn.location.docker.DockerContainerLocation;
+
+import com.google.common.base.Predicates;
+import com.google.common.collect.Iterables;
+
+public class VanillaDockerApplicationImpl extends VanillaSoftwareProcessImpl implements VanillaDockerApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(VanillaDockerApplicationImpl.class);
+
+    @Override
+    public void init() {
+        log.info("Starting Docker application id {}", getId());
+    }
+
+    @Override
+    protected void preStart() {
+        setAttribute(DockerContainer.CONTAINER_ID, getDockerContainer().getContainerId());
+    }
+
+    @Override
+    protected void connectSensors() {
+        connectServiceUpIsRunning();
+    }
+
+    public DockerContainer getDockerContainer() {
+        DockerContainerLocation location = (DockerContainerLocation) Iterables.find(getLocations(), Predicates.instanceOf(DockerContainerLocation.class));
+        return location.getOwner();
+    }
+
+    public DockerHost getDockerHost() {
+        return getDockerContainer().getDockerHost();
+    }
+
+    @Override
+    public void disconnectSensors() {
+        disconnectServiceUpIsRunning();
+    }
+
+    @Override
+    public Class<? extends VanillaDockerApplicationDriver> getDriverInterface() {
+        return VanillaDockerApplicationDriver.class;
+    }
+
+}
