19d526db045933b53c3628d6758bd51417c66cf7
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-09-03 19:22:09 +0100

    Formatting Docker catalog entry config keys

--- examples/src/main/java/brooklyn/clocker/example/DockerCloud.java
+++ examples/src/main/java/brooklyn/clocker/example/DockerCloud.java
@@ -35,27 +35,27 @@ import brooklyn.util.time.Duration;
         iconUrl="classpath://docker-top-logo.png")
 public class DockerCloud extends AbstractApplication {
 
-    @CatalogConfig(label="Docker Version", priority=0)
+    @CatalogConfig(label="Docker Version", priority=90)
     public static final ConfigKey<String> DOCKER_VERSION = ConfigKeys.newConfigKeyWithDefault(SoftwareProcess.SUGGESTED_VERSION, "1.2");
 
-    @CatalogConfig(label="Location Name", priority=1)
+    @CatalogConfig(label="Location Name", priority=80)
     public static final ConfigKey<String> LOCATION_NAME = ConfigKeys.newConfigKeyWithDefault(
             DockerInfrastructure.LOCATION_NAME.getConfigKey(), "my-docker-cloud");
 
-    @CatalogConfig(label="Security Group (Optional)", priority=1)
+    @CatalogConfig(label="Security Group (Optional)", priority=70)
     public static final ConfigKey<String> SECURITY_GROUP = ConfigKeys.newConfigKeyWithDefault(
             DockerInfrastructure.SECURITY_GROUP, "");
 
-    @CatalogConfig(label="Host Cluster Minimum Size", priority=1)
+    @CatalogConfig(label="Host Cluster Minimum Size", priority=60)
     public static final ConfigKey<Integer> DOCKER_HOST_CLUSTER_MIN_SIZE = ConfigKeys.newConfigKeyWithDefault(DockerInfrastructure.DOCKER_HOST_CLUSTER_MIN_SIZE, 2);
 
-    @CatalogConfig(label="Container Cluster Maximum Size", priority=2)
+    @CatalogConfig(label="Maximum Containers per Host", priority=50)
     public static final ConfigKey<Integer> DOCKER_CONTAINER_CLUSTER_MAX_SIZE = ConfigKeys.newConfigKeyWithDefault(DockerHost.DOCKER_CONTAINER_CLUSTER_MAX_SIZE, 4);
 
-    @CatalogConfig(label="Container Cluster Maximum CPU Usage", priority=2)
+    @CatalogConfig(label="Maximum CPU Usage per Host", priority=40)
     public static final ConfigKey<Double> DOCKER_CONTAINER_CLUSTER_MAX_CPU = ConfigKeys.newConfigKeyWithDefault(DockerHost.DOCKER_CONTAINER_CLUSTER_MAX_CPU, 0.5d);
 
-    @CatalogConfig(label="Enable Host HA Policies", priority=2)
+    @CatalogConfig(label="Enable Host HA Policies", priority=30)
     public static final ConfigKey<Boolean> HA_POLICY_ENABLE = DockerHost.HA_POLICY_ENABLE;
 
     @Override
