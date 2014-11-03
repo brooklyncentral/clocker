c2db224fc8ccb605a57bf58295213684306c0c5a
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-11-03 15:11:44 +0000

    Fix build errors

--- docker/src/main/java/brooklyn/entity/container/policy/AvailableContainersScalingEnricher.java
+++ docker/src/main/java/brooklyn/entity/container/policy/AvailableContainersScalingEnricher.java
@@ -2,27 +2,25 @@ package brooklyn.entity.container.policy;
 
 import java.util.Map;
 
-import com.google.common.base.Function;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-import com.google.common.reflect.TypeToken;
-
 import brooklyn.config.ConfigKey;
 import brooklyn.enricher.basic.AbstractEnricher;
-import brooklyn.entity.Entity;
 import brooklyn.entity.basic.ConfigKeys;
 import brooklyn.entity.basic.EntityLocal;
 import brooklyn.entity.container.docker.DockerInfrastructure;
 import brooklyn.event.AttributeSensor;
 import brooklyn.event.SensorEvent;
 import brooklyn.event.SensorEventListener;
+import brooklyn.location.docker.strategy.BreadthFirstPlacementStrategy;
 import brooklyn.policy.autoscaling.AutoScalerPolicy;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.reflect.TypeToken;
 
 public class AvailableContainersScalingEnricher extends AbstractEnricher {
 
     ConfigKey<ConfigKey<Integer>> MAX_CONTAINERS = ConfigKeys.newConfigKey(new TypeToken<ConfigKey<Integer>>() {},
-            "enricher.availability.containers", "Sensor producing the number of containers in the cluster",
-            DockerInfrastructure.DOCKER_CONTAINER_CLUSTER_MAX_SIZE);
+            "enricher.availability.containers", "The maximum number of containers per host in the cluster",
+            BreadthFirstPlacementStrategy.DOCKER_CONTAINER_CLUSTER_MAX_SIZE);
 
     ConfigKey<AttributeSensor<Integer>> CONTAINERS = ConfigKeys.newConfigKey(new TypeToken<AttributeSensor<Integer>>() {},
             "enricher.availability.containers", "Sensor producing the number of containers in the cluster",
