f0f3b225e8ba459c0c7e17e859f774301d5b369a
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-11-10 16:51:05 +0000

    Update presentation of container info

--- console/src/main/webapp/js/controllers.js
+++ console/src/main/webapp/js/controllers.js
@@ -25,7 +25,7 @@ clocker.controller('infrastructures', function ($scope, $http, $interval) {
   }, 5000);
 });
 
-clocker.controller('hosts', function ($scope, $rootScope, $http, $interval) {
+clocker.controller('hosts', function ($scope, $rootScope, $http) {
   $scope.applicationId = $scope.infrastructure.id;
   var current = $scope.infrastructure.children[0].children[0].children.filter(function(value) {
     return value.type == 'brooklyn.entity.container.docker.DockerHost';
@@ -41,16 +41,24 @@ clocker.controller('hosts', function ($scope, $rootScope, $http, $interval) {
       if (typeof $rootScope.collapsed[value.id] == 'undefined') {
         $rootScope.collapsed[value.id] = true;
       }
-    });
-    if ($scope.hosts[value.id]) {
       $http.get('/v1/applications/' + $scope.applicationId + '/entities/' + value.id + '/config/current-state').success(function(data) {
         $scope.hosts[value.id].config = data;
       });
-    }
+    });
   });
 });
 
-clocker.controller('containers', function ($scope, $http, $interval) {
+clocker.controller('sparklines', function ($scope, $rootScope, $http, $interval) {
+  $interval(function() {
+    $http.get('/v1/applications/' + $scope.infrastructure.id + '/descendants/sensor/machine.loadAverage?typeRegex=brooklyn.entity.container.docker.DockerHost').success(function(data) {
+      $scope.infrastructures = data.filter(function(value) {
+        return value.children[0].type == 'brooklyn.entity.container.docker.DockerInfrastructure';
+      });
+    });
+  }, 1000);
+});
+
+clocker.controller('containers', function ($scope, $http) {
   $http.get('/v1/applications/' + $scope.applicationId + '/entities/' + $scope.host.id + '/children').success(function(data) {
     $scope.containers = { };
     var cluster = data.filter(function(value) {
