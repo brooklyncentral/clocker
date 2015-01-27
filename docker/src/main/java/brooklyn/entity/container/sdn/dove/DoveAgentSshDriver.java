d3eb6b71907f534e6796f60ebe21a3518c49abda
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2015-01-27 18:55:39 +0000

    Updated SDN configuration

--- docker/src/main/java/brooklyn/entity/container/sdn/dove/DoveAgentSshDriver.java
+++ docker/src/main/java/brooklyn/entity/container/sdn/dove/DoveAgentSshDriver.java
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import brooklyn.entity.basic.AbstractSoftwareProcessSshDriver;
 import brooklyn.entity.basic.Entities;
 import brooklyn.entity.basic.EntityLocal;
+import brooklyn.entity.basic.lifecycle.ScriptHelper;
 import brooklyn.entity.container.sdn.SdnAgent;
 import brooklyn.entity.software.SshEffectorTasks;
 import brooklyn.location.basic.SshMachineLocation;
@@ -25,9 +26,11 @@ import brooklyn.util.net.Urls;
 import brooklyn.util.ssh.BashCommands;
 import brooklyn.util.task.DynamicTasks;
 import brooklyn.util.task.Tasks;
-import brooklyn.util.text.Strings;
 import brooklyn.util.text.StringPredicates;
+import brooklyn.util.text.Strings;
 
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Preconditions;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
@@ -79,9 +82,8 @@ public class DoveAgentSshDriver extends AbstractSoftwareProcessSshDriver impleme
         Networking.checkPortsValid(getPortMap());
 
         String routes = getEntity().getAttribute(DoveAgent.DOCKER_HOST).execCommand("netstat -r");
-        LOG.debug("Current routes: " + routes);
-        String address = Strings.getFirstWord(Iterables.find(Splitter.on("\n").split(routes), StringPredicates.containsLiteral("255.255.255.192")));
-        String gateway = Strings.getFirstWordAfter("10.0.0.0", Iterables.find(Splitter.on("\n").split(routes), StringPredicates.containsLiteral("10.0.0.0")));
+        String address = Strings.getFirstWord(Iterables.find(Splitter.on(CharMatcher.anyOf("\r\n")).split(routes), StringPredicates.containsLiteral("255.255.255.192")));
+        String gateway = Strings.getFirstWordAfter(Iterables.find(Splitter.on(CharMatcher.anyOf("\r\n")).split(routes), StringPredicates.containsLiteral("10.0.0.0")), "10.0.0.0");
         LOG.debug("Found gateway {} and address {}", gateway, address);
 
         List<String> commands = Lists.newLinkedList();
@@ -91,20 +93,12 @@ public class DoveAgentSshDriver extends AbstractSoftwareProcessSshDriver impleme
         commands.add(sudo(String.format("ifconfig br_mgmt_1:1 %s netmask 255.255.255.192", address)));
         commands.add(sudo("brctl addif br_mgmt_1 eth0"));
         commands.add(sudo(String.format("route add -net 10.0.0.0 netmask 255.0.0.0 gw %s", gateway)));
-        commands.add("/etc/init.d/libvirtd start");
+        commands.add(sudo("service libvirtd start"));
+        commands.add(BashCommands.executeCommandThenAsUserTeeOutputToFile("echo \"nameserver 8.8.8.8\"", "root", "/etc/resolv.conf"));
 
         newScript(CUSTOMIZING)
                 .body.append(commands)
                 .execute();
-
-        String bridge = getEntity().getAttribute(DoveAgent.DOCKER_HOST).execCommand(BashCommands.sudo("brctl show"));
-        String bridgeId = Strings.getFirstWordAfter("dovebr_", bridge);
-        String addBridgeCommand = String.format("echo \"{ \"vnid_list\": \"8064181\", \"ip_addr\": \"%s\" }\" | curl -i -u admin:admin -X POST -H \"Content-Type: application/json\" -d @- http://%s/api/dove/vrmgr/vnids/vm_mgr",
-                bridgeId, getEntity().getAttribute(SdnAgent.SDN_AGENT_ADDRESS).getHostAddress());
-        LOG.debug("Add bridge command: " + addBridgeCommand);
-
-        String addBridgeOutput = getEntity().getAttribute(DoveAgent.DOCKER_HOST).execCommand(addBridgeCommand);
-        LOG.debug("Added bridge: " + addBridgeOutput);
 
         String doveXmlTemplate = processTemplate(entity.getConfig(DoveNetwork.CONFIGURATION_XML_TEMPLATE));
         String savedDoveXml = Urls.mergePaths(getRunDir(), "dove.xml");
@@ -118,21 +112,35 @@ public class DoveAgentSshDriver extends AbstractSoftwareProcessSshDriver impleme
     @Override
     public void launch() {
         newScript(MutableMap.of(USE_PID_FILE, false), LAUNCHING)
-                .body.append(BashCommands.sudo("start doved"))
+                .body.append(sudo("start doved"))
                 .execute();
+
+        Integer bridgeId = getEntity().getConfig(DoveNetwork.DOVE_BRIDGE_ID);
+        String addBridgeCommand = String.format("echo \"{ \\\"vnid_list\\\": \\\"%d\\\", \\\"ip_addr\\\": \\\"%s\\\" }\" | "
+                + "curl -i -u admin:admin -X POST -H \"Content-Type: application/json\" -d @- http://%s/api/dove/vrmgr/vnids/vm_mgr",
+                bridgeId, getEntity().getAttribute(SdnAgent.SDN_AGENT_ADDRESS).getHostAddress(), getEntity().getConfig(DoveNetwork.DOVE_CONTROLLER).getHostAddress());
+        String addBridgeOutput = getEntity().getAttribute(DoveAgent.DOCKER_HOST).execCommand(addBridgeCommand);
+        Preconditions.checkState(Strings.containsLiteral(addBridgeOutput, "successfully"), "Failed to export network %s", bridgeId);
+
+        String bridge = getEntity().getAttribute(DoveAgent.DOCKER_HOST).execCommand(sudo("brctl show"));
+        String doveBridge = Strings.getFirstWordAfter(bridge, "dovebr_");
+        LOG.debug("Added bridge: " + doveBridge);
     }
 
     @Override
     public boolean isRunning() {
-        return newScript(MutableMap.of(USE_PID_FILE, false), CHECK_RUNNING)
-                .body.append(BashCommands.sudo("status doved | grep PID"))
-                .execute() == 0;
+        ScriptHelper helper = newScript(MutableMap.of(USE_PID_FILE, false), CHECK_RUNNING)
+                .body.append(sudo("status doved"))
+                .noExtraOutput()
+                .gatherOutput();
+        helper.execute();
+        return helper.getResultStdout().contains("running");
     }
 
     @Override
     public void stop() {
         newScript(MutableMap.of(USE_PID_FILE, false), STOPPING)
-                .body.append(BashCommands.sudo("stop doved"))
+                .body.append(sudo("stop doved"))
                 .execute();
     }
 
