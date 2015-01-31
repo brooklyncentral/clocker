b1f0093b379299073d0880f7141e9a223c420719
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2015-01-31 18:42:43 +0000

    Fix merge issue with old Weave config keys

--- docker/src/test/java/brooklyn/entity/container/docker/DockerInfrastructureTests.java
+++ docker/src/test/java/brooklyn/entity/container/docker/DockerInfrastructureTests.java
@@ -43,7 +43,7 @@ public class DockerInfrastructureTests {
     public static DockerInfrastructure deployAndWaitForDockerInfrastructure(TestApplication app, Location location) {
         DockerInfrastructure dockerInfrastructure = app.createAndManageChild(EntitySpec.create(DockerInfrastructure.class)
                 .configure(DockerInfrastructure.DOCKER_HOST_CLUSTER_MIN_SIZE, 1)
-                .configure(DockerInfrastructure.WEAVE_ENABLED, true)
+                .configure(DockerInfrastructure.SDN_ENABLE, false)
                 .displayName("Docker Infrastructure"));
         LOG.info("Starting {} in {}", dockerInfrastructure, location);
         app.start(ImmutableList.of(location));
