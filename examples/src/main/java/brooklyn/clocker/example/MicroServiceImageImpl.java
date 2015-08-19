eabe30dfc59e375c711d7fe47aec3e805948e8fe
Andrea Turli <andrea.turli@gmail.com>
2015-08-19 11:12:49 +0200

    enhance MicroServiceImage
    - if the my-docker-cloud is not available, it will first install a DockerInfrastructure with a single DockerHost

--- examples/src/main/java/brooklyn/clocker/example/MicroServiceImageImpl.java
+++ examples/src/main/java/brooklyn/clocker/example/MicroServiceImageImpl.java
@@ -15,21 +15,53 @@
  */
 package brooklyn.clocker.example;
 
+import java.util.Collection;
+
 import org.apache.brooklyn.api.entity.proxying.EntitySpec;
+import org.apache.brooklyn.api.location.Location;
+import org.apache.brooklyn.util.collections.MutableMap;
+
+import com.google.common.collect.ImmutableList;
 
 import brooklyn.entity.basic.AbstractApplication;
+import brooklyn.entity.basic.Entities;
+import brooklyn.entity.container.docker.DockerInfrastructure;
 import brooklyn.entity.container.docker.application.VanillaDockerApplication;
+import brooklyn.entity.trait.Startable;
+import brooklyn.location.docker.DockerLocation;
 
 public class MicroServiceImageImpl extends AbstractApplication implements MicroServiceImage {
 
+    private static final String MICRO_SERVICE_LOCATION = "my-docker-cloud";
+    DockerInfrastructure dockerInfrastructure = null;
+    VanillaDockerApplication vanillaDockerApplication = null;
+
     @Override
     public void initApp() {
-        addChild(EntitySpec.create(VanillaDockerApplication.class)
+        if (getManagementContext().getLocationRegistry().getDefinedLocationByName(MICRO_SERVICE_LOCATION) == null) {
+            dockerInfrastructure = addChild(EntitySpec.create(DockerInfrastructure.class)
+                    .configure(DockerInfrastructure.DOCKER_HOST_CLUSTER_MIN_SIZE, 1)
+                    .configure(DockerInfrastructure.SDN_ENABLE, false)
+                    .configure(DockerInfrastructure.LOCATION_NAME, MICRO_SERVICE_LOCATION)
+                    .displayName("Docker Infrastructure"));
+        }
+        vanillaDockerApplication = addChild(EntitySpec.create(VanillaDockerApplication.class)
                 .configure("containerName", config().get(CONTAINER_NAME))
                 .configure("imageName", config().get(IMAGE_NAME))
                 .configure("imageTag", config().get(IMAGE_TAG))
                 .configure("openPorts", config().get(OPEN_PORTS))
                 .configure("directPorts", config().get(DIRECT_PORTS)));
+    }
+
+    @Override
+    protected void doStart(Collection<? extends Location> locations) {
+
+        if (dockerInfrastructure != null) {
+            Entities.invokeEffector(this, dockerInfrastructure, Startable.START, MutableMap.of("locations", getLocations())).getUnchecked();
+        }
+
+        DockerLocation dockerLocation = (DockerLocation) getManagementContext().getLocationRegistry().resolve(MICRO_SERVICE_LOCATION);
+        vanillaDockerApplication.start(ImmutableList.of(dockerLocation));
     }
 
 }
